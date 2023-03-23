523cf1518e7cea4d9a57a4edb2bf7e17e6f5a9b2
ERMAN ENGINLER <ermaneng@gmail.com>
2023-03-24 01:40:30 +0300

    bootstrap for standalone app.component

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+  
+})
+export class AppComponent {}
