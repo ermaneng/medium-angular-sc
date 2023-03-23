63c2664cd2df45c8812f46215c8f5a97cba57fa8
ERMAN ENGINLER <ermaneng@gmail.com>
2023-03-24 01:52:15 +0300

    added library with sc

---
+++ web/projects/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'lib-my-lib',
+  template: `
+    <p>
+      my-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyLibComponent {
+
+}
--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyLibComponent } from 'my-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MyLibComponent],
   
 })
 export class AppComponent {}
